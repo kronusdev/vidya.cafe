d60c79f6ab3b91e1300e2feeeac256d8c262ccbe
devBoi76 <mrzyglodjan@protonmail.com>
2021-08-12 19:53:36 +0200

    quick fix

--- files/routes/settings.py
+++ files/routes/settings.py
@@ -177,7 +177,7 @@ def changelogsub(v):
 @validate_formkey
 def namecolor(v):
 	color = str(request.form.get("color", "")).strip()
-	if color not in ['ff66ac','805ad5','62ca56','38a169','80ffff','2a96f3','eb4963','ff0000','f39731','30409f','3e98a7','e4432d','7b9ae4','ec72de','7f8fa6', 'f8db58']: abort(400)
+	if color not in ['ff66ac','805ad5','62ca56','38a169','80ffff','2a96f3','eb4963','ff0000','f39731','30409f','3e98a7','e4432d','7b9ae4','ec72de','7f8fa6', 'f8db58', 'ffffff']: abort(400)
 	v.namecolor = color
 	g.db.add(v)
 	return redirect("/settings/profile")
@@ -197,7 +197,7 @@ def themecolor(v):
 @validate_formkey
 def titlecolor(v):
 	titlecolor = str(request.form.get("titlecolor", "")).strip()
-	if titlecolor not in ['ff66ac','805ad5','62ca56','38a169','80ffff','2a96f3','eb4963','ff0000','f39731','30409f','3e98a7','e4432d','7b9ae4','ec72de','7f8fa6', 'f8db58']: abort(400)
+	if titlecolor not in ['ff66ac','805ad5','62ca56','38a169','80ffff','2a96f3','eb4963','ff0000','f39731','30409f','3e98a7','e4432d','7b9ae4','ec72de','7f8fa6', 'f8db58', 'ffffff']: abort(400)
 	v.titlecolor = titlecolor
 	g.db.add(v)
 	return redirect("/settings/profile")
