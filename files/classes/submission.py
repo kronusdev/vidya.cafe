4c772fd9b2c89a44b6241eb034caa888d6e803b6
devBoi76 <mrzyglodjan@protonmail.com>
2021-08-13 14:05:34 +0200

    even simpler

--- files/classes/submission.py
+++ files/classes/submission.py
@@ -114,7 +114,7 @@ class Submission(Base, Stndrd, Age_times, Scores):
 	@property
 	@lazy
 	def score_active(self):
-		return (self.score + self.comment_count)
+		return self.comment_count
 
 	@property
 	@lazy
