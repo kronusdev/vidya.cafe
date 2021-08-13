f2768c3ce0f4b5fa1db13bd4002e333427cb9ff9
devBoi76 <mrzyglodjan@protonmail.com>
2021-08-13 22:06:04 +0200

    uhh

--- files/routes/settings.py
+++ files/routes/settings.py
@@ -149,7 +149,12 @@ def settings_profile_post(v):
 	theme = request.values.get("theme")
 	if theme:
 		v.theme = theme
-		if theme =="hackernews": v.themecolor = "ff6600"
+		if theme == "coffee": v.themecolor = "805ad5"
+		elif theme == "tron": v.themecolor = "adf1d2"
+		elif theme == "4chan": v.themecolor = "7f8fa6"
+		elif theme =="light": v.themecolor = "805ad5"
+		elif theme =="hackernews": v.themecolor = "ff6600"
+
 		g.db.add(v)
 		return "", 204
 
