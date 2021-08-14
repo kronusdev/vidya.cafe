2b76f5dad76c98c271b085d2509dcf301679f7cc
devBoi76 <mrzyglodjan@protonmail.com>
2021-08-14 13:19:54 +0200

    Revert "settings win98 color n theme"
    This reverts commit 050a37d3924094d1fbfe0439e3620c34aa39545e.

--- files/routes/settings.py
+++ files/routes/settings.py
@@ -151,9 +151,6 @@ def settings_profile_post(v):
 		v.theme = theme
 		if theme == "coffee": v.themecolor = "805ad5"
 		elif theme =="hackernews": v.themecolor = "ff6600"
-		elif theme == "win98": 
-			v.themecolor = "00007b"
-			v.background = "other/bg-5"
 
 		g.db.add(v)
 		return "", 204
@@ -182,7 +179,7 @@ def changelogsub(v):
 @validate_formkey
 def namecolor(v):
 	color = str(request.form.get("color", "")).strip()
-	if color not in ['ff66ac','805ad5','62ca56','38a169','80ffff','2a96f3','eb4963','ff0000','f39731','30409f','3e98a7','e4432d','7b9ae4','ec72de','7f8fa6', 'f8db58', 'ffffff', 'ff6600', '00007b']: abort(400)
+	if color not in ['ff66ac','805ad5','62ca56','38a169','80ffff','2a96f3','eb4963','ff0000','f39731','30409f','3e98a7','e4432d','7b9ae4','ec72de','7f8fa6', 'f8db58', 'ffffff', 'ff6600']: abort(400)
 	v.namecolor = color
 	g.db.add(v)
 	return redirect("/settings/profile")
@@ -192,7 +189,7 @@ def namecolor(v):
 @validate_formkey
 def themecolor(v):
 	themecolor = str(request.form.get("themecolor", "")).strip()
-	if themecolor not in ['ffffff', '47a3ff','805ad5','62ca56','38a169','80ffff','2a96f3','eb4963','ff0000','f39731','adf1d2','3e98a7','e4432d','7b9ae4','ec72de','7f8fa6', 'f8db58', 'ff6600', '00007b']: abort(400)
+	if themecolor not in ['ffffff', '47a3ff','805ad5','62ca56','38a169','80ffff','2a96f3','eb4963','ff0000','f39731','adf1d2','3e98a7','e4432d','7b9ae4','ec72de','7f8fa6', 'f8db58', 'ff6600']: abort(400)
 	v.themecolor = themecolor
 	g.db.add(v)
 	return redirect("/settings/profile")
@@ -202,7 +199,7 @@ def themecolor(v):
 @validate_formkey
 def titlecolor(v):
 	titlecolor = str(request.form.get("titlecolor", "")).strip()
-	if titlecolor not in ['ff66ac','805ad5','62ca56','38a169','80ffff','2a96f3','eb4963','ff0000','f39731','30409f','3e98a7','e4432d','7b9ae4','ec72de','7f8fa6', 'f8db58', 'ffffff', 'ff6600', '00007b']: abort(400)
+	if titlecolor not in ['ff66ac','805ad5','62ca56','38a169','80ffff','2a96f3','eb4963','ff0000','f39731','30409f','3e98a7','e4432d','7b9ae4','ec72de','7f8fa6', 'f8db58', 'ffffff', 'ff6600']: abort(400)
 	v.titlecolor = titlecolor
 	g.db.add(v)
 	return redirect("/settings/profile")
