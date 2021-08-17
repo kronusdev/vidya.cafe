27ff79522785f4c37bcd43bfbaea9b2c0ebe6c80
kronusdev <kronusdev@protonmail.com>
2021-08-17 11:08:24 -0500

    Update submission.py

--- files/classes/submission.py
+++ files/classes/submission.py
@@ -35,7 +35,7 @@ class Submission(Base, Stndrd, Age_times, Scores):
 	id = Column(BigInteger, primary_key=True)
 	submission_aux = relationship(
 		"SubmissionAux",
-		lazy="subquery",
+		lazy="joined",
 		uselist=False,
 		innerjoin=True,
 		primaryjoin="Submission.id==SubmissionAux.id")
