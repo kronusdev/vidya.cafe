7a21b21e68a1c67b5e8031fca4659431ae9a58ac
kronusdev <kronusdev@protonmail.com>
2021-08-18 08:36:35 -0500

    Revert "whoops"
    This reverts commit f777c1930885917dc71ea82bee5d7bfdbebd2573.

--- files/templates/steamauth/SteamConfig.php
+++ files/templates/steamauth/SteamConfig.php
@@ -2,8 +2,8 @@
 //Version 4.0
 $steamauth['apikey'] = getenv('STEAM_KEY'); // Your Steam WebAPI-Key found at https://steamcommunity.com/dev/apikey
 $steamauth['domainname'] = "http://vidya.cafe/"; // The main URL of your website displayed in the login page
-$steamauth['logoutpage'] = "settings_profile.php"; // Page to redirect to after a successfull logout (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
-$steamauth['loginpage'] = "settings_profile.php"; // Page to redirect to after a successfull login (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
+$steamauth['logoutpage'] = "settings_profile.html"; // Page to redirect to after a successfull logout (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
+$steamauth['loginpage'] = "settings_profile.html"; // Page to redirect to after a successfull login (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
 
 // System stuff
 if (empty($steamauth['apikey'])) {die("<div style='display: block; width: 100%; background-color: red; text-align: center;'>SteamAuth:<br>Please supply an API-Key!<br>Find this in steamauth/SteamConfig.php, Find the '<b>\$steamauth['apikey']</b>' Array. </div>");}
