e41ed30e626ed612fd2c497338bc9f52e5994768
Panjkrc <panjkrc.mihael@gmail.com>
2021-08-19 21:21:24 +0200

    Update donate.py

--- files/routes/donate.py
+++ files/routes/donate.py
@@ -1,8 +1,10 @@
 from flask import request
 from files.__main__ import app
+import json
 
 @app.get("/ko-fi")
 @app.post("/ko-fi")
 def kofi():
 	print(request.form['data'])
+	print(json.loads(request.form['data']))
 	return "OK", 200
