c90a943fdc08ef067c32d55fbba28836ff451323
devBoi76 <mrzyglodjan@protonmail.com>
2021-08-25 19:07:14 +0200

    a

--- compile-css-linux.sh
+++ compile-css-linux.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-sass ./files/assets/style/dark.scss ./files/assets/style/dark_47a3ff.css &
+sass ./files/assets/style/dark.scss ./files/assets/style/dark_47a3ff.css
 echo "Finished dark"
-sass ./files/assets/style/light.scss ./files/assets/style/light_47a3ff.css &
+sass ./files/assets/style/light.scss ./files/assets/style/light_47a3ff.css
 echo "Finished light"
-sass ./files/assets/style/blur.scss ./files/assets/style/blur_47a3ff.css &
+sass ./files/assets/style/blur.scss ./files/assets/style/blur_47a3ff.css
 echo "Finished blur"
-sass ./files/assets/style/win98.scss ./files/assets/style/win98_47a3ff.css &
+sass ./files/assets/style/win98.scss ./files/assets/style/win98_47a3ff.css
 echo "Finished win98"
-sass ./files/assets/style/hackernews.scss ./files/assets/style/hackernews_47a3ff.css &
+sass ./files/assets/style/hackernews.scss ./files/assets/style/hackernews_47a3ff.css
 echo "Finished HN"
-sass ./files/assets/style/coffee.scss ./files/assets/style/coffee_47a3ff.css &
+sass ./files/assets/style/coffee.scss ./files/assets/style/coffee_47a3ff.css
 echo "Finished coffe"
 sass ./files/assets/style/tron.scss ./files/assets/style/tron_47a3ff.css
 echo "Finished tron"
