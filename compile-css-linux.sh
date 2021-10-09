a9c9cd08f0fa1aad80d01e2257d8d553b44ef5c2
devBoi76 <mrzyglodjan@protonmail.com>
2021-10-09 16:12:44 +0200

    change compile script

--- compile-css-linux.sh
+++ compile-css-linux.sh
@@ -1,22 +1,5 @@
 #!/bin/sh
-sass ./files/assets/style/dark.scss ./files/assets/style/dark_47a3ff.css
-echo "Finished dark"
-sass ./files/assets/style/light.scss ./files/assets/style/light_47a3ff.css
-echo "Finished light"
-sass ./files/assets/style/blur.scss ./files/assets/style/blur_47a3ff.css
-echo "Finished blur"
-sass ./files/assets/style/win98.scss ./files/assets/style/win98_47a3ff.css
-echo "Finished win98"
-sass ./files/assets/style/hackernews.scss ./files/assets/style/hackernews_47a3ff.css
-echo "Finished HN"
-sass ./files/assets/style/coffee.scss ./files/assets/style/coffee_47a3ff.css
-echo "Finished coffe"
-sass ./files/assets/style/tron.scss ./files/assets/style/tron_47a3ff.css
-echo "Finished tron"
-sass ./files/assets/style/4chan.scss ./files/assets/style/4chan_47a3ff.css
-echo "Finished 4chan"
-sass ./files/assets/style/midnight.scss ./files/assets/style/midnight_47a3ff.css
-echo "Finished midnight"
+sass ./files/assets/style/main2.scss ./files/assets/style/main.css
+echo "Finished compilation"
 
-python3 ./compilecss.py
 rm ./files/assets/style/*[.]map
