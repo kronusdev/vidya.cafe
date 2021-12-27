80eee8c0a34deb905eb3158059ae963dc36353b8
devBoi76 <mrzyglodjan@protonmail.com>
2021-12-27 16:29:20 +0100

    add badges to badge_defs

--- seed-db.sql
+++ seed-db.sql
@@ -1,1 +1,15 @@
+INSERT INTO public.badge_defs VALUES (1, 'Valued Customer', 'Is a valued and quality member of vidya.cafe :)', 'valued-customer.png', 3, 10, NULL);
 INSERT INTO public.badge_defs VALUES (6, 'Beta User', 'Joined during open beta', 'beta.png', 4, 3, NULL);
+INSERT INTO public.badge_defs VALUES (7, 'Donator', 'Donated any amount of money to vidya.cafe', 'donator.png', 1, 69, NULL);
+INSERT INTO public.badge_defs VALUES (6, 'Bug Spotter', 'Found and reported an issue on the site', 'sitebreaker.png', 3, 4, NULL);
+INSERT INTO public.badge_defs VALUES (9, '25 Sips', 'Have 25 sips', '25sips.png', 1, 1, 'v.coins >= 25 and v.coins < 50');
+INSERT INTO public.badge_defs VALUES (10, '50 Sips', 'Have 50 sips', '50sips.png', 1, 2, 'v.coins >= 50 and v.coins < 100');
+INSERT INTO public.badge_defs VALUES (11, '100 Sips', 'Have 100 sips', '100sips.png', 1, 3, 'v.coins >= 100 and v.coins < 150');
+INSERT INTO public.badge_defs VALUES (12, '150 Sips', 'Have 150 sips', '150sips.png', 1, 4, 'v.coins >= 150 and v.coins < 250');
+INSERT INTO public.badge_defs VALUES (13, '250 Sips', 'Have 250 sips', '250sips.png', 1, 5, 'v.coins >= 250 and v.coins < 500');
+INSERT INTO public.badge_defs VALUES (14, '500 Sips', 'Have 500 sips', '500sips.png', 1, 6, 'v.coins >= 500 and v.coins < 1000');
+INSERT INTO public.badge_defs VALUES (15, '1000 Sips', 'Have 1000 sips', '1000sips.png', 1, 7, 'v.coins >= 1000 and v.coins < 2000');
+INSERT INTO public.badge_defs VALUES (16, '2000 Sips', 'Have 2000 sips', '2000sips.png', 1, 8, 'v.coins >= 2000 and v.coins < 5000');
+INSERT INTO public.badge_defs VALUES (17, '5000 Sips', 'Have 5000 sips', '5000sips.png', 1, 9, 'v.coins >= 5000');
+INSERT INTO public.badge_defs VALUES (18, 'Halloween 2021', 'A reward for the 2021 Spooky Stuffs Halloween competition winner', 'halloween-2021.png', 4, 5, NULL);
+INSERT INTO public.badge_defs VALUES (19, 'Christmas 2021', 'Merry Christmas, may your vidya be fun and your season bright <3', 'xmas2021.png', 4, 5, NULL);
