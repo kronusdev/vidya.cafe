343e2b76716cf213ae23091b05c86054bd5233a7
devBoi76 <mrzyglodjan@protonmail.com>
2022-02-11 22:00:27 +0100

    keyboard navigation for posts (WIP) the shortcut to enable it is ctrl+alt+l then use up and down arrows

--- files/assets/js/keyboardnav.js
+++ files/assets/js/keyboardnav.js
@@ -1,23 +1,74 @@
+let post_changer = {
+    enabled: false,
+    posts: [],
+    current: undefined
+}
+
+// let post_changing_mode = false
+
 document.addEventListener("keydown", (event) => {
-    if(event.ctrlKey) {
-        switch(event.key) {
+    if (event.ctrlKey) {
+        switch (event.key) {
             case "ArrowRight": {
                 let next = document.getElementById("page-next").getAttribute("href");
                 if (next === null) {
-                    break;
+                    return;
                 }
                 window.location.href = next;
                 console.log("right")
-                break; 
+                return;
             }
             case "ArrowLeft": {
                 let prev = document.getElementById("page-prev").getAttribute("href");
                 if (prev === null) {
-                    break;
+                    return;
                 }
                 window.location.href = prev;
                 console.log("left")
+                return;
+            }
+        }
+    }
+    if (event.ctrlKey && event.altKey) {
+        switch (event.key) {
+            case "l": {
+                // enter post selection mode (navigate with arrow up/down)
+                let posts = document.querySelectorAll("#posts .card[tabindex='-1']")
+                console.log('l')
+                // get first visible post
+                let first_post = undefined
+                let idx = undefined
+                for(let i = 0; i < posts.length; i++) {
+                    if (posts[i].getBoundingClientRect().top < 0) {
+                        
+                    } else {
+                        first_post = posts[i+1];
+                        idx = i+1;
+                        break;
+                    }
+                }
+
+                first_post.focus()
+                post_changer.enabled = true
+                post_changer.posts = posts
+                post_changer.current = idx
+            }
+        }
+    }
+    if (post_changer.enabled) {
+        switch (event.key) {
+            case "ArrowDown": {
+                if (post_changer.current != post_changer.posts.length) {
+                    post_changer.posts[post_changer.current].nextElementSibling.nextElementSibling.focus()
+                    post_changer.current += 1
+                }
                 break;
+            }
+            case "ArrowUp": {
+                if (post_changer.current != 0) {
+                    post_changer.posts[post_changer.current].previousElementSibling.previousElementSibling.focus()
+                    post_changer.current -= 1
+                }
             }
         }
     }
